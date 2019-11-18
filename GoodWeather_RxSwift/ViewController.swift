78cdb76d47c635c80c3ad7d35c7e327b8b409f44
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-18 01:16:36 -0800

    Improving the Project with Driver and ControlProperty

--- GoodWeather_RxSwift/ViewController.swift
+++ GoodWeather_RxSwift/ViewController.swift
@@ -68,14 +68,15 @@ class ViewController: UIViewController {
         let search = URLRequest.load(resource: resource)
             .observeOn(MainScheduler.instance)
             .catchErrorJustReturn(WeatherResult.empty)
-            
+            .asDriver(onErrorJustReturn: WeatherResult.empty)
+        
         search.map{
             "\($0.main.temp) ℉"
-            }.bind(to: self.temperatureLabel.rx.text).disposed(by: disposeBag)
+            }.drive(self.temperatureLabel.rx.text).disposed(by: disposeBag)
         
         search.map{
             "\($0.main.humidity) 💦"
-            }.bind(to: self.humidityLabel.rx.text).disposed(by: disposeBag)
+            }.drive(self.humidityLabel.rx.text).disposed(by: disposeBag)
         
     }
 }
